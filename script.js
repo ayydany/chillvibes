ac1c7a054a0865f6f4efc576024fbe85576a4670
Daniel Carmo <DanyBoss@users.noreply.github.com>
2018-05-20 08:56:22 +0100

    memes

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 
-jQuery(function(){
-    jQuery("#bgndVideo").YTPlayer();
-  });
+$( document ).ready(() => {
+    jQuery(function(){
+        jQuery("#bgndVideo").YTPlayer();
+    });
+      
+    function play() {
+        jQuery('#bgndVideo').YTPPlay();
+        jQuery('#bgndVideo').YTPUnmute();
+    }
+})
